771d91bfd7abc160f3e20b6c6f865fa678bde499
6283726 627262663 <nikolastonge@hotmail.com>
2022-07-05 00:17:10 -0400

    Save button appearance

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,41 +8,8 @@ $(".saveBtn").on("click", function() {
     // get row text field class and row id values
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-
-    // save in localStorage
-    localStorage.setItem(time , value /* text in the textarea */);
-});
-
-// load any saved data from localStorage 
-
-// 9am
-$("#9am-row .description").val(localStorage.getItem("9am-row"));
-
-// 10am
-$("#10am-row .description").val(localStorage.getItem("#10am-row"));
-
-// 11am
-$("#11am-row .description").val(localStorage.getItem("11am-row"));
-
-// 12pm
-$("#12pm-row .description").val(localStorage.getItem("12pm-row"));
-
-// 1pm
-$("#1pm-row .description").val(localStorage.getItem("1pm-row"));
-
-// 2pm
-$("#2pm-row .description").val(localStorage.getItem("2pm-row"));
-
-// 3pm
-$("#3pm-row .description").val(localStorage.getItem("3pm-row"));
-
-// 4pm
-$("#4pm-row .description").val(localStorage.getItem("4pm-row"));
-
-// 5pm
-$("#5pm-row .description").val(localStorage.getItem("5pm-row"));
-
-// converting time and display
+}
+  
 
 var hour9 = 9;
 var hour10 = 10;

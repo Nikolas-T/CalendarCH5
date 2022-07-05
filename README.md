8641496c86a87b2f1fbdf39978ec68731df96553
6283726 627262663 <nikolastonge@hotmail.com>
2022-07-04 23:52:08 -0400

    some more code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CalendarCH5
-challenge 5 calendar application
+challenge 5 workday planner application
+displays current (system) time and conveniently clears the tables after you refresh the page.
+its not a bug, its a feature :)
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,37 +10,37 @@ $(".saveBtn").on("click", function() {
     var time = $(this).parent().attr("id");
 
     // save in localStorage
-    localStorage.setItem(time /* aka KEY = row id */, value /* text in the textarea */);
+    localStorage.setItem(time , value /* text in the textarea */);
 });
 
 // load any saved data from localStorage 
 
 // 9am
-$("#9am-row .description").val(localStorage.getItem("9am-row" /* localStorage KEY also the row id */));
+$("#9am-row .description").val(localStorage.getItem("9am-row"));
 
 // 10am
-$("#10am-row .description").val(localStorage.getItem("#10am-row" /* localStorage KEY also the row id */));
+$("#10am-row .description").val(localStorage.getItem("#10am-row"));
 
 // 11am
-$("#11am-row .description").val(localStorage.getItem("11am-row" /* localStorage KEY also the row id */));
+$("#11am-row .description").val(localStorage.getItem("11am-row"));
 
 // 12pm
-$("#12pm-row .description").val(localStorage.getItem("12pm-row" /* localStorage KEY also the row id */));
+$("#12pm-row .description").val(localStorage.getItem("12pm-row"));
 
 // 1pm
-$("#1pm-row .description").val(localStorage.getItem("1pm-row" /* localStorage KEY also the row id */));
+$("#1pm-row .description").val(localStorage.getItem("1pm-row"));
 
 // 2pm
-$("#2pm-row .description").val(localStorage.getItem("2pm-row" /* localStorage KEY also the row id */));
+$("#2pm-row .description").val(localStorage.getItem("2pm-row"));
 
 // 3pm
-$("#3pm-row .description").val(localStorage.getItem("3pm-row" /* localStorage KEY also the row id */));
+$("#3pm-row .description").val(localStorage.getItem("3pm-row"));
 
 // 4pm
-$("#4pm-row .description").val(localStorage.getItem("4pm-row" /* localStorage KEY also the row id */));
+$("#4pm-row .description").val(localStorage.getItem("4pm-row"));
 
 // 5pm
-$("#5pm-row .description").val(localStorage.getItem("5pm-row" /* localStorage KEY also the row id */));
+$("#5pm-row .description").val(localStorage.getItem("5pm-row"));
 
 // converting time and display
 
